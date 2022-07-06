027316f075e74da2cc0139568bd792a5692bd553
DELL <nitin,patle@outlook.com>
2022-07-06 17:42:47 +0530

    1st commit

---
+++ pythonPrograms/FibonacciSeries.py
@@ -0,0 +1,13 @@
+#Fibopnacci Series:-
+
+num = int(input("Enter Any Number: "))
+n1 = 0
+n2 = 1
+count = 0
+
+while count < num:
+    print(n1)
+    nth = n1 + n2
+    n1 = n2
+    n2 = nth
+    count += 1
